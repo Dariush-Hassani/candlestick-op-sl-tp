c7df1c12ed043279ccbcd3225d1d7f8a059a3b5b
dariush-hassani <dariush.hassani@outlook.com>
2023-09-27 18:45:10 +0330

    mobile debug

--- helper.js
+++ helper.js
@@ -4,14 +4,14 @@ export const getCursorPoint = (id, evt) => {
   let svg = document.querySelector(`#${id}`);
   let pt = svg.createSVGPoint();
   let cursorPoint = (evt) => {
-    if(evt.touches[0]){
+    if (evt.touches && evt.touches[0]) {
       pt.x = evt.touches[0].clientX;
       pt.y = evt.touches[0].clientY;
-    }else{
+    } else {
       pt.x = evt.clientX;
       pt.y = evt.clientY;
     }
- 
+
     return pt.matrixTransform(svg.getScreenCTM().inverse());
   };
 
